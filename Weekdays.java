ad9515f88f660f4dfbbdde62afa7cf8fc00386d0
Muhammad Saif Khan <109592926+MuhammadSaifKhan@users.noreply.github.com>
2022-07-28 17:38:15 +0530

    Add files via upload

---
+++ Weekdays.java
@@ -0,0 +1,31 @@
+//Program that takes a number from the user and generates an integer between 1 and 7
+
+import java.util.Scanner;
+public class Weekdays {
+
+    
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int day = in.nextInt();
+
+        System.out.println(getDayName(day));
+    }
+
+    public static String getDayName(int day) {
+        String dayName = "";
+        switch (day) {
+            case 1: dayName = "Monday"; break;
+            case 2: dayName = "Tuesday"; break;
+            case 3: dayName = "Wednesday"; break;
+            case 4: dayName = "Thursday"; break;
+            case 5: dayName = "Friday"; break;
+            case 6: dayName = "Saturday"; break;
+            case 7: dayName = "Sunday"; break;
+
+        }
+
+        return dayName;
+    }
+}
